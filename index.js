ecb99338665b458b6a10ce20001f177b10198f97
Ezhiloli <ezhiloli07@gmail.com>
2022-10-13 14:29:22 +0530

    Scss setup is done

--- index.js
+++ index.js
@@ -18,6 +18,15 @@ const passportLocal = require('./config/passport-local-strategy');
 
 const MongoStore = require('connect-mongo')(session);
 
+const sassMiddleware = require('node-sass-middleware');
+
+app.use(sassMiddleware({
+    src:'./assets/scss', //
+    dest:'./assets/css',
+    debug:true, //shows the error 
+    outputStyle:'expanded',//output is single line or multi line
+    prefix:'/css'
+}))
 
 
 app.use(express.urlencoded());
